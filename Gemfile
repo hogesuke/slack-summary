9ab199bbd4d39671228f084d08dabc50962bc0be
hogesuke <miyado@gmail.com>
2015-06-20 01:00:18 +0900

    gemやらnode_moduleやらgulpやら導入

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', '~> 1.4.6'
+gem 'sinatra-contrib', '~> 1.4.4'
+gem 'mysql2', '~> 0.3.18'
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+  gulp.src('./sass/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./www/css/'));
+});
+
+gulp.task('js', function () {
+  gulp.src('./www/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/js/min/'));
+});
+
+gulp.task('default', function() {
+  gulp.watch('./sass/*.scss',['sass']);
+  gulp.watch('./www/js/*.js',['js']);
+});
