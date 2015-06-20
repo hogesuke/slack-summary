c3d3369f91b5f1de7841df6a2ef0ca5c14da9bea
hogesuke <miyado@gmail.com>
2015-06-20 11:12:46 +0900

    gulp-sourcemapsの導入

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
   gulp.src('./sass/*.scss')
@@ -12,7 +13,9 @@ gulp.task('sass', function () {
 
 gulp.task('js', function () {
   gulp.src('./www/js/*.js')
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
+    .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('./www/js/min/'));
 });
 
