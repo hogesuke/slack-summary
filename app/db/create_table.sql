0728db12f90a8c52c3dae330249a31a873463a86
hogesuke <miyado@gmail.com>
2015-06-24 00:02:27 +0900

    tableの仮組み

--- app/db/create_table.sql
+++ app/db/create_table.sql
@@ -3,9 +3,34 @@ CREATE DATABASE IF NOT EXISTS slack_summary;
 USE slack_summary;
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
-  slack_id VARCHAR(32),
+  id INT AUTO_INCREMENT,
+  slack_id VARCHAR(32) NOT NULL,
   name VARCHAR(32),
   created_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+  id INT AUTO_INCREMENT,
+  user_id INT,
+  channel_id VARCHAR(32),
+  title VARCHAR(64),
+  created_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INT AUTO_INCREMENT,
+  channel_id VARCHAR(32),
+  ts CHAR(17),
+  text VARCHAR(5000),
+  created_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS articles_messages (
+  article_id INT,
+  message_id INT,
+  created_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (article_id, message_id)
 );
